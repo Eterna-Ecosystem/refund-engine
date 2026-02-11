6aa4f15ef5120d2440cc7baf0e42e0a1faff8fcf
Dgov626 <eternapartnerships@gmail.com>
2026-02-11 18:17:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
